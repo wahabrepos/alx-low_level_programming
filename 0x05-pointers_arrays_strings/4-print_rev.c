acd1b6cec10de05624aa200aa6d06d01f4526386
cabdala68 <aragasanmo@gmail.com>
2022-10-24 15:04:01 -0700

    function for reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,22 @@ void print_rev(char *s)
 	int i = 0;
 	int len;
 
-	for (len = 0; s[len] != 0; len++)
+	while (*s != '\0')
+	
 	{
-			}
-	for (i = len - 1; i >= 0; i--)
-							{
-	_putchar(s[i]);
-					}
-	_putchar(10);
+		
+	i++;
+	s++;
+	}
+	
+	s--;
+	
+	for (len = i; len > 0; len--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+
 }
