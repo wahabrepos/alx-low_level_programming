f3bfd589a5116ac69ddc014c9fd370598d4fb70b
cabdala68 <aragasanmo@gmail.com>
2022-10-24 06:40:07 -0700

    C program that prints char with puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char ch[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(ch);
+
+	return (0);
+}
