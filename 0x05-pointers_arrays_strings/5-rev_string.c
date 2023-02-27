77f11e28e17cae33e5ac7607dae4b0afed992c89
cabdala68 <aragasanmo@gmail.com>
2023-02-27 14:16:17 -0800

    apply betty style

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,14 +11,14 @@ void rev_string(char *s)
 	int counter = 0;
 	int i;
 
-	while(s[counter] != '\0')
-		counter ++;
-	for(i=0; i<counter; i++)
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
 	{
-	counter --;
-	rev =s[i];
-	s[i]= s[counter];
-	s[counter]= rev;
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 
 	}
 }
