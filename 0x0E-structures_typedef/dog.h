d7ceaade74a06ab13a5a00c001211e3acc25abf7
cabdala68 <aragasanmo@gmail.com>
2023-03-20 15:38:10 -0700

    defining struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,33 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - Details of a dog.
+ * @name: Pointer to dog's name.
+ * @age: Age of dog.
+ * @owner: Pointer to owner's name.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/**
+ * dog_t - Entry point
+ *
+ * Description: Synonym for struct dog
+ */
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
