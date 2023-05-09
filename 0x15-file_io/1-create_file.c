b820492c790e8a405e857563a1eaf07cd2e00166
cabdala68 <aragasanmo@gmail.com>
2023-05-08 19:37:02 -0700

    fix bugs

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int numlet;
+	int rdwrt;
+
+	if (!filename)
+	return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+	return (-1);
+
+	if (!text_content)
+	text_content = "";
+
+	for (numlet = 0; text_content[numlet]; numlet++);
+	rdwrt = write(fd, text_content, numlet);
+
+	if (rdwrt == -1)
+	return (-1);
+
+	close(fd);
+
+	return (1);
+}
