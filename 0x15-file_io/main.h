31b485e63953c237f761fbfa65cae857b3fc14cf
cabdala68 <aragasanmo@gmail.com>
2023-05-08 19:32:41 -0700

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,8 +2,15 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H */
+#endif
 
